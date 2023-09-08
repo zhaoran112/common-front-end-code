6012e06d1a9cf1bfbc83b3b525acbfcbb2f5bb62
“zhaoran112 <835750627@qq.com>
2023-09-08 15:52:23 +0800

    文件下载

--- js/download.js
+++ js/download.js
@@ -66,6 +66,19 @@ downloadByUrl({
     target: '_self',
 });
 
+function dataURLtoBlob(base64Buf) {
+    const arr = base64Buf.split(",");
+    const typeItem = arr[0];
+    const mime = typeItem.match(/:(.*?);/)[1];
+    const bstr = window.atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+}
+
 // base64流下载
 export function downloadByBase64(buf, filename, mime, bom) {
     const base64Buf = dataURLtoBlob(buf);
